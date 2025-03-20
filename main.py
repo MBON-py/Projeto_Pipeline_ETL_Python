fe13f6539a44b972dfdcc79cc57b0ac34cce4d9e
Maria Benussy <mariabenussy@googlemail.com>
2025-03-20 13:41:03 -0300

    Criação da aplicação de validação de dados

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import pandas as pd
 #Análise Exploratória de Dados (EDA) com Pandas Profiling
 from ydata_profiling import ProfileReport
 
-df = pd.read_csv('precos-glp-02_2025_edit.csv', sep=";", encoding="utf-8", skipinitialspace=True, decimal=",", engine='python')
+df = pd.read_csv('precos-glp-02_2025.csv', sep=";", encoding="utf-8", skipinitialspace=True, decimal=",", engine='python')
 profile = ProfileReport(df, title="Análise das vendas de GLP no Brasil em FEV-25")
 profile.to_file("output.html")
